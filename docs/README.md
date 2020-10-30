30eaf4c304740e6ed33f6181b2dc49f6977e4e6d
Upendar <u.gareri@gmail.com>
2020-10-30 16:32:50 +0100

    Add autoscaling feature

--- docs/README.md
+++ docs/README.md
@@ -2,4 +2,16 @@
 
 - While launching the EC2 instance, we have the option to pass a shell script that will get executed during the EC2 instance boot. That option is in the form of `user_data` argument.
 
-- 
+## Things to keep in mind or are required while creating ASG (AutoScaling Group)
+
+1. aws_launch_configuration
+    - Similar to instance creation code
+    - lifecycle rule (any change in this config resource will be immutable replace and as asg resource refers to this, there will be problem in replacing. Hence, `create_before_destroy` attribute is required in launch configuration)
+
+2. aws_autoscaling_group
+    - References launch configuration
+    - here we give min and max size
+
+3. vpc_zone_identifier
+    - it's better to keep your instances in different subnets as they reside in different AZs to be safe from any data outage in one subnet/AZ
+    - to put your instances in different subnets we use `data` sources to get info
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-east-2"
+    region = "us-east-2"  # Ohio
 }
 
 # provide_type name { attributes }
@@ -12,7 +12,7 @@ resource "aws_instance" "example_terra" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, world" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
     tags = {
@@ -25,9 +25,70 @@ resource "aws_security_group" "instance-1-sg" {
     name = "terraform-example-1-sg"
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]  # allow any public ip address
     }
 }
+
+# input variable
+variable "server_port" {
+    description = "To access the server, use this port"
+    type = number
+    default = 8080
+}
+
+# output variable
+output "public_ip" {
+    value = aws_instance.example_terra.public_ip
+    description = "Public ip address of the server"
+    
+}
+
+# -------------- AUTOSCALING GROUP -------------
+
+# # launch configuration
+# resource "aws_launch_configuration" "example" {
+#     image_id = ""
+#     instance_type = "t2.micro"
+
+#     security_groups = [aws_security_group.instance-1-sg.id]
+
+#     user_data = <<-EOF
+#                 #!/bin/bash
+#                 echo "Hello, world" > index.html
+#                 nohup busybox httpd -f -p ${var.server_port} &
+#                 EOF
+#     lifecycle {
+#         create_before_destroy = true
+#     }
+# }
+
+# # asg
+# resource "aws_autoscaling_group" "example" {
+#     launch_configuration = aws_launch_configuration.example.name
+#     vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+#     min_size = 2
+#     max_size = 10
+
+#     tag {
+#         key = "Name"
+#         value = "terraform-asg-example"
+#         propagate_at_launch = true
+#     }
+
+# }
+
+# # data sources
+
+# # vpc
+# data "aws_vpc" "default" {
+#     default = true  # only filter
+# }
+
+# # subnets
+# data "aws_subnet_ids" "default" {
+#     vpc_id = data.aws_vpc.default.id
+# }
