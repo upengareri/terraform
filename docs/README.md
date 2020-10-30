c21ab20c5622fe4f1dade40d95164def73968048
Upendar <u.gareri@gmail.com>
2020-10-30 12:54:34 +0100

    Add terraform doc

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# Notes on Terraform
+
+- While launching the EC2 instance, we have the option to pass a shell script that will get executed during the EC2 instance boot. That option is in the form of `user_data` argument.
+
+- 
--- main.tf
+++ main.tf
@@ -2,11 +2,32 @@ provider "aws" {
     region = "us-east-2"
 }
 
+# provide_type name { attributes }
 resource "aws_instance" "example_terra" {
     ami           = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
+    # add security group (implicit dependency)
+    vpc_security_group_ids = [aws_security_group.instance-1-sg.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, world" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags = {
-        Name = "terraform-example"
+        Name = "terraform-instance-1"
+    }
+}
+
+# create security group
+resource "aws_security_group" "instance-1-sg" {
+    name = "terraform-example-1-sg"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]  # allow any public ip address
     }
 }
